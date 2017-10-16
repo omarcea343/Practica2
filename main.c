1b596d5761b4e70e48f989e8caad80fd27837e7a
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-10-16 13:48:00 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -7,28 +7,32 @@ int main(){
 	srand(time(NULL));
 	
 	char respuesta;
+	char carta1 = ' ';
+	int carta2 = 0;
 	int valor = 0, valor1 = 0, minimo = 10, maximo = 14;
 	
 	valor = minimo + rand() / (RAND_MAX / (maximo - minimo + 1) + 1);
 	valor1 = minimo + rand() / (RAND_MAX / (maximo - minimo + 1) + 1);
 	
+	if(valor > 10){
+		switch(valor){
+			case 11:
+				carta1 = 'J';
+			case 12:
+				carta1 = 'Q';
+			case 13:
+				carta1 = 'K';
+			case 14:
+				carta1 = 'A';
+		}
+	}
+	
+	printf("Tus cartas son: %c y &d \n", carta1, carta2);
+	
 	printf("Quieres otra carta? S/N \n");
 	scanf("%c", &respuesta);
 	
 	printf("\nSeleccion: %c \n", respuesta);
-	
-	if(valor > 10){
-		switch(valor){
-			case 11:
-				carta1:'J'
-			case 12:
-				carta1:'Q'
-			case 11:
-				carta1:'K'
-		}
-	}
-	
-	
 	
 	return 0;
 }
